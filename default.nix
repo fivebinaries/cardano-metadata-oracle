affd1fc24a512108664de025d88da6d4293ce18f
slowbackspace <slowbackspace@gmail.com>
2021-05-02 18:58:31 +0200

    Revert "chore: nix plugin"
    This reverts commit f96ef023e2731af6dc277c8bbaae4d34a3456959.

--- default.nix
+++
@@ -1,6 +0,0 @@
-# This is a minimal `default.nix` by yarn-plugin-nixify. You can customize it
-# as needed, it will not be overwritten by the plugin.
-
-{ pkgs ? import <nixpkgs> { } }:
-
-pkgs.callPackage ./yarn-project.nix { } { src = ./.; }
